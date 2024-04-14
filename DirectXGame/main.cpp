1b83e46a299e639f2fe43f7713506028a0410ed8
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-04-14 15:28:23 +0900

    01_01_確認課題

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"LE2B_04_オザワ_ユウト_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
